33b377f915de1031a22e4689265132381877e727
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-10-26 19:53:10 -0500

    Fix: CORS config

--- statsprime/settings.py
+++ statsprime/settings.py
@@ -79,12 +79,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
     "https://statsprime.vercel.app",
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'statsprime.urls'
 
