ad2ed545cf09d3ec72cc35d4e26c3f905a167d97
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-10-26 21:19:39 -0500

    Configurar ssl para local

--- statsprime/settings.py
+++ statsprime/settings.py
@@ -108,7 +108,11 @@ WSGI_APPLICATION = 'statsprime.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'), 
+        conn_max_age=600, 
+        ssl_require=True,
+    )
 }
 
 
