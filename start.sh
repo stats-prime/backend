45669f47084d8c61083a6507edd97e64e64fc0c0
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-10-27 14:59:04 -0500

    Limpieza base de datos para evitar duplicados

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+echo "Creando tablas"
+python manage.py makemigrations --noinput
 
 echo "Aplicando migraciones..."
 python manage.py migrate --noinput
 
 echo "Recopilando archivos estáticos..."
 python manage.py collectstatic --noinput
+
+echo "📥 Cargando fixtures de farm..."
+python manage.py loaddata farm/fixtures/farm_games.json || echo "⚠️ No se pudo cargar farm_games.json"
+python manage.py loaddata farm/fixtures/farm_sources_genshin.json || echo "⚠️ No se pudo cargar farm_sources_genshin.json"
+python manage.py loaddata farm/fixtures/farm_rewards_genshin.json || echo "⚠️ No se pudo cargar farm_rewards_genshin.json"
 
 echo "🚀 Iniciando servidor..."
 gunicorn statsprime.wsgi:application --bind 0.0.0.0:$PORT
